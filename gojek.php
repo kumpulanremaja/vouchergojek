f19eedc43c2228acf1b33cfe314f516ecbd150e8
kumpulanremaja <47444671+kumpulanremaja@users.noreply.github.com>
2019-11-10 22:29:51 +0700

    Update gojek.php

--- gojek.php
+++ gojek.php
@@ -7,9 +7,11 @@ if (!file_exists('token')) {
 
 include ("curl.php");
 echo "\n";
-echo "\e[94m            NOT SAFE FOR WORK IF2               \n";
+echo "\e[94m          Voucher Claim Gojek           \n";
 echo "\e[91m FORMAT NOMOR HP : INDONESIA '62***' , US='1***'\n";
 echo "\e[93m SCRIPT GOJEK AUTO REGISTER + AUTO CLAIM VOUCHER\n";
+echo "\e[93m SCRIPT : Kumpulanremaja.com\n";
+echo "\e[93m Github: 4kumpulanremaja \n";
 echo "\n";
 echo "\e[96m[?] Masukkan Nomor HP Anda (62/1) : ";
 $nope = trim(fgets(STDIN));
